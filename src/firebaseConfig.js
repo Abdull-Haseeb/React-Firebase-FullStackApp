c6e9b064a64841ff867fc36d27c6694d2e1ea9a9
Abdull Haseeb <abdullhaseebonline@gmail.com>
2023-09-17 21:33:26 +0500

    firebase config

---
+++ src/firebaseConfig.js
@@ -0,0 +1,12 @@
+// firebaseConfig.js
+// import dotenv from "./.env"
+const firebaseConfig = {
+  apiKey: "dotenv.YOUR_API_KEY",
+  authDomain: "dotenv.YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID",
+};
+
+export default firebaseConfig;
